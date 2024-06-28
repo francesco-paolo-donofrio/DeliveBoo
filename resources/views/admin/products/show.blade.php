b6a1157ef01d68e43b1f386c59cc35838759adcd
ElisaMavilia <elisamavilia1@gmail.com>
2024-06-28 13:02:53 +0200

    fix

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -2,15 +2,16 @@
 @section('title', $product->name)
 @section('content')
 
-<section>
+
     <div div class="py-4 ps-4">
         <h1>{{$product->name}}</h1>
-        <!-- Qui va immagine con src in asset  -->
+        ciao
+        <!-- Qui va l'immagine con src in asset  -->
         <!-- <img src="{{asset('storage/' . $product->image)}}" alt="{{$product->name}}" -->
         <p>{{$product->description}}</p>
         <div>
            <!--  Qui va la rotta di modifica con relativo bottone  -->
         </div>
 </div>
-</section>
+
 @endsection
