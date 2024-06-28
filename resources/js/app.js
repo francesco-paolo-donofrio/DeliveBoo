8f04586824b6398e1952d838d7b4b082757b95a7
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-28 13:05:39 +0200

    class loaded + css searchbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,14 @@ import "./bootstrap";
 import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**", "../fonts/**"]);
+
+
+document.getElementById('search-icon').addEventListener('click', function() {
+    const searchBar = document.getElementById('search-bar');
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+        searchBar.focus();
+    } else {
+        searchBar.style.display = 'none';
+    }
+});
