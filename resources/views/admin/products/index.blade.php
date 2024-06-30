ff63aa83e23ad3f18def8bcf58c41c8fea483c57
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-30 23:03:57 +0200

    Table started

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -28,13 +28,13 @@
             <td>{{$product->price}}</td>
             <td>{{$product->visible}}</td>
             <td>
-                <a href="{{route('admin.products.show', $product->id)}}" title="Show" class="text-black px-2"><i class="fa-solid fa-eye"></i></a>
-                <a href="{{route('admin.products.edit', $product->id)}}" title="Edit" class="text-black px-2"><i class="fa-solid fa-pen"></i></a>
+                <a href="{{route('admin.products.show', $product->id)}}" title="Show" class="text-black px-2"><i class="fa-solid fa-eye" style="color: #758C20;"></i></a>
+                <a href="{{route('admin.products.edit', $product->id)}}" title="Edit" class="text-black px-2"><i class="fa-solid fa-pen" style="color: white;"></i></a>
                 <form action="{{route('admin.products.destroy', $product->id)}}" method="POST" class="d-inline-block">
                     @csrf
                     @method('DELETE')
                     <button type="submit" class="delete-button border-0 bg-transparent"  data-item-title="{{ $product->name }}" data-item-id = "{{ $product->id }}">
-                    <i class="fa-solid fa-trash"></i>
+                    <i class="fa-solid fa-trash" style="color: #e70d18;"></i>
                     </button>
                 </form>
             </td>
