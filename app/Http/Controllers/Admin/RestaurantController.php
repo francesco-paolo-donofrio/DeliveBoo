026c99e3b1bb7d66ea4ec72b04406e8bc5720dda
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-07-03 15:57:30 +0200

    fixed main image

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -50,12 +50,10 @@ class RestaurantController extends Controller
         
         $user = Auth::user();
         $user_restaurant = $user->restaurant;        
-        if (!$user_restaurant) {
-            return view('admin.restaurants.index', compact('user_restaurant'));
-        }
+        
         $user_products = $user->restaurant->products->all();
         $user_types = $user->restaurant->types->all();
-        
+      
         return view('admin.restaurants.index', compact('user_restaurant', 'user_products', 'restaurant', 'user_types'));
     }
     /**
