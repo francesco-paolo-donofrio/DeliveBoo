162ff89d129b84639ecca47985d8b8dfb18a2cbc
Frankosviz <geom.francescopaolo@gmail.com>
2024-07-15 15:34:53 +0200

    commit test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
         <h1>Pannello amministratore</h1>
         <h3>Effettua la registrazione per creare il tuo ristorante e gestire i tuoi ordini</h3>
         <div class="nav-item text-center bounce-title">
-            <a class="btn btn-outline-success" href="{{ route('register') }}"><h5>Registrati <i class="fas fa-user-plus fs-4 p-2"></i></h5>
+            <a class="f-d-button-form-edit" href="{{ route('register') }}"><h5>Registrati <i class="fas fa-user-plus fs-4 p-2"></i></h5>
             </a>
         </div>
         <h4>oppure, se hai già un account, effettua il login</h4>
         <div class="nav-item text-center bounce-title">
-            <a class="btn btn-outline-success" href="{{ route('login') }}"><h5>Login <i class="fas fa-user fs-4 p-2"></i></h5>
+            <a class="f-d-button-form-edit" href="{{ route('login') }}"><h5>Login <i class="fas fa-user fs-4 p-2"></i></h5>
             </a>
         </div>
     </div>
